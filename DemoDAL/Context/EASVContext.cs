1e84b02ba236b06e0b9b54d46e684a8f8ee37492
CrystalOfLife <nicolai7@godmail.dk>
2017-10-25 11:01:54 +0200

    customerrepository

--- DemoDAL/Context/EASVContext.cs
+++ DemoDAL/Context/EASVContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DemoDAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DemoDAL.Context
 {
@@ -11,7 +12,7 @@ namespace DemoDAL.Context
             base.OnModelCreating(modelBuilder);
         }
 
-        //public DbSet<Entity> Entites { get; set; }
-       
+        public DbSet<Customer> Customers { get; set; }
+
     }
 }
---
+++ DemoDAL/Repositories/ICustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using DemoDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoDAL.Repositories
+{
+    interface ICustomerRepository : IRepository<Customer>
+    {
+    }
+}
